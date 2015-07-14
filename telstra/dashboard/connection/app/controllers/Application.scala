91e7834d44787a11c07dedf3fca5a91fae5a3ce0
manish-kumar8856 <manish.kumar@globalsoft.com>
2015-07-14 12:14:46 +0530

    scala code for db connection and fetch data from db

---
+++ telstra/dashboard/connection/app/controllers/Application.scala
@@ -0,0 +1,27 @@
+package controllers
+ 
+import play.api._
+import play.api.mvc._
+import play.api.Play.current
+import scala.collection.mutable.MutableList
+import models.CharacterSet
+ 
+object Application extends Controller {
+ 
+  def index = Action {
+ 
+    import play.api.db._
+ 
+    val list = MutableList[CharacterSet]()
+ 
+    DB.withConnection { conn =>
+      val stm = conn.createStatement()
+      val res = stm.executeQuery("""select * from emp""")
+      while (res.next()) {
+        list.+=(CharacterSet(res.getString(1), res.getString(2)))
+      }
+    }
+ 
+    Ok(views.html.index(list))
+  }
+}
---
+++ telstra/dashboard/connection/build.sbt
@@ -0,0 +1,21 @@
+name := """connection"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.6"
+
+libraryDependencies ++= Seq(
+  jdbc,
+  cache,
+  ws,
+  specs2 % Test
+)
+
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+
+// Play provides two styles of routers, one expects its actions to be injected, the
+// other, legacy style, accesses its actions statically.
+// routesGenerator := InjectedRoutesGenerator
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.27"
