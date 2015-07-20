19a7f46b9dcf43e32636682932aeb1e649336857
Surya Melarkode <surya.melarkode@globalsoft.com>
2015-07-20 19:40:55 +0530

    Updated UI Design with Content Column and the Graph Column, Updated the graph to a stacked bar, set culling to false, modified data to show the stack bar, moved legends and axis labels

--- telstra/dashboard_ui/js/graph.js
+++ telstra/dashboard_ui/js/graph.js
@@ -1,5 +1,13 @@
+
+
 var timelineChart = c3.generate({
+    padding: {
+        bottom: 60
+    },
     bindto: '#timelineChart',
+    size: {
+        height: 768
+    },
     data: {
         x: 'datetime',
         columns: [
@@ -17,27 +25,50 @@ var timelineChart = c3.generate({
             new Date('2015-07-18 08:15:45'),new Date('2015-07-25 08:15:45'),new Date('2015-08-02 08:15:45'),new Date('2015-08-03 08:15:45'),
             new Date('2015-08-10 08:15:45'),new Date('2015-08-17 08:15:45'),new Date('2015-08-25 08:15:45'),new Date('2015-08-02 08:15:45'),
             new Date('2015-09-12 08:15:45'),new Date('2015-09-28 08:15:45'),new Date('2015-10-21 08:15:45'),new Date('2015-10-28 08:15:45')],
-            ['LIVECHAT 1.0 - Success', 0,73117,0,85464,11641,27142,0,0,48570,53984,40376,60053,0,0,37622,32488,41790,69382,88847,61405,15976,11366,25711,72473,0,71497,25066,76537,94690,0,14891,0,88776,15300,74774,79094,5905,16254,34219 ],
-            ['LIVECHAT 1.0 - Warning', 26911,0,0,0,0,0,58778,0,0,0,0,0,68838,0,37622,0,41790,0,0,61405,0,0,0,0,0,0,0,0,0,0,0,52756,0,0,0,0,0,0,0 ],
-            ['LIVECHAT 1.0 - Failed', 0,0,31789,0,0,0,0,22655,48570,0,0,0,0,89571,0,0,0,0,0,0,0,0,0,0,22366,0,0,0,0,60353,0,0,0,0,0,0,0,0,0 ]
+            ['LIVECHAT 1.0 - Success', 6000,53117,15000,85464,11641,27142,5000,5000,48570,53984,40376,60053,5000,5000,37622,32488,41790,69382,88847,61405,15976,11366,25711,72473,0,71497,25066,76537,94690,5000,14891,0,88776,15300,74774,79094,5905,16254,34219],
+            ['LIVECHAT 1.0 - Warning', 20911, 10000,5000,5000,5000,5000,58778,5000,5000,5000,5000,5000,68838,5000,37622,5000,41790,5000,0,61405,0,0,0,0,5000,0,0,0,0,5000,0,52756,0,0,0,5000,0,0,0 ],
+            ['LIVECHAT 1.0 - Failed', 6000,10000,31789,5000,5000,5000,5000,22655,48570,5000,5000,5000,5000,89571,5000,5000,5000,5000,5000,5000,5000,5000,5000,0,22366,0,0,0,0,60353,0,0,0,0,0,0,0,0,0 ]
         ],
-        type: 'bar'
+        type: 'bar',
+        groups: [
+            ['LIVECHAT 1.0 - Success', 'LIVECHAT 1.0 - Warning', 'LIVECHAT 1.0 - Failed']
+        ]
+    },
+    legend: {
+        position: 'right'
+    },
+    grid: {
+        x: {
+            show: true
+        },
+        y: {
+            show: true
+        }
     },
     color: {
-        pattern: ['#1b5e20','#f57f17','#bf360c']
+           pattern: ['#84BA5B','#E1974C','#D35E60']
     },
     axis: {
         x: {
-            label: 'Date',
+            label: {
+                text:'Date',
+                position: 'outer-center'
+            },
             type: 'timeseries',
             tick: {
-                format: '%Y-%m-%d'
+                rotate: -55,
+                multiline: false,
+                format: '%Y-%m-%d',
+                culling: false
                 // for normal axis, default on
                 // for category axis, default off
             }
         },
         y: {
-            label: 'Record Count'
+            label: {
+                text:'Record Count',
+                position: 'outer-middle'
+            }
         }
     },
     regions: [
@@ -58,7 +89,6 @@ var timelineChart = c3.generate({
         enabled: true
     }
 });
-
 
 var gaugeChart = c3.generate({
     bindto: '#gaugeChart',
