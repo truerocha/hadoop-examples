800c42b698baad0d44959de3e6fe8d25295fbee2
manish-kumar8856 <manish.kumar@globalsoft.com>
2015-08-03 16:33:47 +0530

    Pulling Data Through Scala

--- telstra/backup/connection/app/controllers/Application.scala
+++ telstra/backup/connection/app/controllers/Application.scala
@@ -16,9 +16,25 @@ object Application extends Controller {
  
     DB.withConnection { conn =>
       val stm = conn.createStatement()
-      val res = stm.executeQuery("""select * from actor""")
+      val res = stm.executeQuery("""select
+	  split_str(sds.location, '/', 9) as "SOURCE_SYSTEM"
+	, parts.part_name as "SOURCE_LINEAGE"
+from dbs
+inner join tbls
+	on tbls.db_id = dbs.db_id
+inner join sds
+	on sds.sd_id = tbls.sd_id
+inner join partitions parts
+	on parts.tbl_id = tbls.tbl_id
+where 
+	substr(sds.location, 1, 22) = 'hdfs://tdcdv2/data/tdc'
+	and split_str(sds.location, '/', 8) = 'base'
+	and split_str(sds.location, '/', 9) = 'livechat'
+	and parts.part_name LIKE "%src_date=____-__-__%"
+order by
+	1, 2""")
       while (res.next()) {
-        list.+=(CharacterSet(res.getString(1), res.getString(2),res.getString(3),res.getString(4)))
+        list.+=(CharacterSet(res.getString(1), res.getString(2)))
       }
     }
  
